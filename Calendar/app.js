fa12bd4236c68a42de7e18bf9d25ec9bef7b0f3e
unknown <lyamzin505@gmail.com>
2022-01-10 16:20:57 +0300

    1001

--- Calendar/app.js
+++ Calendar/app.js
@@ -1,14 +1,17 @@
 let monthIndex = 0;
+let events = localStorage.getItem('planList') ? JSON.parse(localStorage.getItem('planList')) : [];
+
+
 const dateArr = (dt) => {
     return dt.toLocaleDateString("en-us", {
         weekday: 'short',
         month: 'short',
-        year: 'numeric',        
+        year: 'numeric',
         day: 'numeric',
-}).split(' ')
+    }).split(' ')
 }
 
-function makeDateObject(weekday, month, day,  year) {
+function makeDateObject(weekday, month, day, year) {
     return {
         'weekday': weekday.slice(0, -1),
         'month': month,
@@ -45,39 +48,66 @@ const weekdays = [
 
 
 const makeTable = () => {
+
     if (calendar.querySelector('.day')) {
         calendar.querySelectorAll('.day').forEach(item => item.remove())
     }
     const date = (new Date())
     date.setMonth(date.getMonth() + monthIndex)
     const dateObject = makeDateObject(...dateArr(date))
-    document.querySelector('.table-zone_month').textContent = `${(date.toLocaleDateString('ru', {month: 'long'}).slice(0, 1).toUpperCase())}${(date.toLocaleDateString('ru', {month: 'long'}).slice(1))} ${dateObject.year}`  
+    document.querySelector('.table-zone_month').textContent = `${(date.toLocaleDateString('ru', {month: 'long'}).slice(0, 1).toUpperCase())}${(date.toLocaleDateString('ru', {month: 'long'}).slice(1))} ${dateObject.year}`
     const year = date.getFullYear();
     const month = date.getMonth();
 
     const firstDayOfTheMonth = makeDateObject(...dateArr(new Date(year, month, 1)));
     const lastDayOfTheMonth = makeDateObject(...dateArr(new Date(year, month + 1, 1)))
-    const daysInMonth = (new Date(year, month + 1, 0)).getDate( );
+    const daysInMonth = (new Date(year, month + 1, 0)).getDate();
     const prePaddingDays = weekdays.indexOf(firstDayOfTheMonth.weekday);
     let postPaddingDays = weekdays.length - weekdays.indexOf(lastDayOfTheMonth.weekday)
     const lastDayOfThePreviousMonth = (new Date(year, month, 0)).getDate();
+
+
     if (postPaddingDays === 7) {
         postPaddingDays = 0
     }
     for (let i = 1; i <= prePaddingDays + daysInMonth + postPaddingDays; i++) {
-    const day = document.createElement('div');
-    day.classList.add('day');
+        let dayString = `${year}-${month + 1}-${i-prePaddingDays}`;
+        if (month.toString().length < 2) {
+            dayString = `${year}-0${month + 1}-${i-prePaddingDays}`
+        }
+
+        const day = document.createElement('div');
+        day.classList.add('day');
         if (i <= prePaddingDays) {
             day.textContent = `${lastDayOfThePreviousMonth - prePaddingDays + i}`
         }
         if (i > prePaddingDays && i <= prePaddingDays + daysInMonth) {
+            const dayPlans = events.filter(item => item.date === dayString)
+
+            dayPlans.sort((a, b) => a.time > b.time ? 1 : -1)
             day.textContent = `${i - prePaddingDays}`
-            day.classList.add('day_active')
-        }    
+            day.classList.add('day_active');
+            if (dayPlans.length > 0) {
+                const dayPlanLink = document.createElement('button');
+                dayPlanLink.textContent = `Задачи (${dayPlans.length})`
+                day.append(dayPlanLink)
+                const dayPlanList = (document.querySelector('#plan-template').content).cloneNode(true);
+                dayPlanList.querySelector('.dayplan_list_heading').textContent = `Задачи на ${dayString}`
+
+                dayPlans.forEach(item => {
+                    const dayPlanDiv = document.createElement('div');
+                    console.log(dayPlanDiv);
+                    dayPlanDiv.classList.add('day-plan');
+                    dayPlanDiv.textContent = `${item.time} ${item.title}`
+                    dayPlanList.querySelector('.dayplan_list-div').append(dayPlanDiv)
+                })
+                day.append(dayPlanList);
+            }
+        }
         if (i > prePaddingDays + daysInMonth) {
             day.textContent = `${i - prePaddingDays - daysInMonth}`
         }
-    calendar.querySelector('.calendary-table').append(day);
+        calendar.querySelector('.calendary-table').append(day);
     }
 }
 
@@ -94,4 +124,42 @@ document.querySelector('#monthDown').addEventListener('click', (e) => {
     e.preventDefault()
     monthIndex--;
     makeTable()
+})
+
+const closeModal = () => {
+    document.querySelector('.create-plan_background').classList.remove('create-plan_background_active')
+}
+
+document.querySelector('.table-header_button').addEventListener('click', () => {
+    document.querySelector('.create-plan_background').classList.add('create-plan_background_active')
+})
+
+document.querySelector('.create-plan_background').addEventListener('click', (e) => {
+    if (e.target.classList.contains('create-plan_background') || e.target === document.querySelector('#cancel-btn')) {
+        closeModal()
+    }
+})
+
+const modalForm = document.querySelector('.create-plan_modal')
+const modalErr = modalForm.querySelector('.modal-error')
+modalForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const modalDate = modalForm.querySelector('input[name=date-inp]');
+    const modalTIme = modalForm.querySelector('input[name=time-inp]');
+    const modalPlanTitle = modalForm.querySelector('input[name=date-title]');
+    if (!modalDate.value || !modalTIme.value || !modalPlanTitle.value) {
+        modalErr.style.opacity = '1'
+    } else {
+        const newPlan = {
+            date: `${modalDate.value}`,
+            time: `${modalTIme.value}`,
+            title: `${modalPlanTitle.value}`,
+            inf: `${modalForm.querySelector('textarea[name=text-inp]').value}`
+        }
+        events.push(newPlan)
+
+        localStorage.setItem('planList', JSON.stringify(events));
+        makeTable();
+        closeModal();
+    }
 })
